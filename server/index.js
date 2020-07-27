7a10305ae4822ebc307cacc1737c8dfd67b48faa
Omar Ahmed <omarpirate2010@yahoo.com>
2020-07-27 13:12:58 +0200

    Connect the database to the server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
+
 
 // Middleware
 app.use(cors());
@@ -9,3 +11,21 @@ app.use(express.json());
 app. listen(5000, () => {
     console.log("server started on port 5000");
 });
+
+// Routes
+
+
+// create todo
+
+
+
+// get all todos
+
+
+// get a todo
+
+
+// update a todo
+
+
+// delete a todo
