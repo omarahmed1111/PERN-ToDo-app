e96d73e51d06c2ea75822d314c1c882a9258c344
Omar Ahmed <omarpirate2010@yahoo.com>
2020-07-27 14:47:00 +0200

    Initialize the client side

--- server/index.js
+++ server/index.js
@@ -33,6 +33,7 @@ app.post("/todos", async (req,res) => {
 app.get("/todos", async (req,res) => {
     try {
         const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
     } catch(err) {
         console.error(err.message);
     }
